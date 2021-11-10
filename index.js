23188fa3cd05bff31b96c97478aa1c7351024237
Yudo Kartiko <yudoworks@gmail.com>
2021-11-10 22:54:14 +0700

    receive data

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
     {
         id: 1,
@@ -30,7 +32,7 @@ app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {    
     const id = Number(request.params.id)
     const note = notes.find(note => note.id === id)
 
@@ -46,6 +48,33 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
 
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
+
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if(!body.content){
+        return response.status(404).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId()
+    }
+
+    notes.concat(note)
+
+    response.json(note)
 })
 
 const PORT = 3001
