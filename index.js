73f09baf4c1192b9032f38fadfbc5c7e1eb9b461
Yudo Kartiko <yudoworks@gmail.com>
2021-11-30 18:24:45 +0700

    remove notes and generatedId function

--- index.js
+++ index.js
@@ -23,27 +23,6 @@ const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
-
 const requestLogger = (request, response, next) => {
   console.log('Method', request.method)
   console.log('Path: ', request.path)
@@ -85,12 +64,6 @@ app.delete('/api/notes/:id', (request, response) => {
   Note.deleteOne({ id: request.params.id }).then(deletedNote => response.json(deletedNote))
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
@@ -98,7 +71,6 @@ app.post('/api/notes', (request, response, next) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
   })
 
   note.save().then(savedNote => {
